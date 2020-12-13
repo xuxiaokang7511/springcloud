be1fb99374811b9e8a43f3c428ab427bbbc772d5
徐小康 <2393189428@qq.com>
2020-12-13 17:47:19 +0800

    eureka 注册与集群配置

--- springcloud-eureka02/src/main/java/com.eureka02/Eureka02Application.java
+++ springcloud-eureka02/src/main/java/com.eureka02/Eureka02Application.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class eureka02Application {
+public class Eureka02Application {
     public static void main(String[] args) {
-        SpringApplication.run(eureka02Application.class,args);
+            SpringApplication.run(Eureka02Application.class, args);
     }
 }
